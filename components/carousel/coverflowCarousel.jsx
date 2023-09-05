00c26658193c75b499eef064bc342ac0b83eeb37
mansijoshi17 <mansijoshi1777@gmail.com>
2023-09-05 10:26:02 +0530

    changes in image

--- components/carousel/coverflowCarousel.jsx
+++ components/carousel/coverflowCarousel.jsx
@@ -10,36 +10,35 @@ import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../firebase";
 
 const CoverflowCarousel = () => {
+  const [userData, setUserData] = useState([]);
 
-  const [userData,setUserData]= useState([]);
-
-  const getUsers= async()=>{
-    const arry =[];
-		const q = query(collection(db, "Users"));
-		const querySnapshot = await getDocs(q);
-		querySnapshot.forEach((fire) => { 
-			const id = fire.id;
-      arry.push({ ...fire.data(), id })  
-		})
+  const getUsers = async () => {
+    const arry = [];
+    const q = query(collection(db, "Users"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((fire) => {
+      const id = fire.id;
+      arry.push({ ...fire.data(), id });
+    });
     setUserData(arry);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getUsers();
-  },[])
+  }, []);
 
   return (
-    <> 
-      <div className="relative px-6 pb-16 sm:px-0"> 
+    <>
+      <div className="relative px-6 pb-16 sm:px-0">
         <Swiper
-          breakpoints={{ 
-            100: { 
+          breakpoints={{
+            100: {
               slidesPerView: 1,
             },
-            575: { 
+            575: {
               slidesPerView: 3,
-            }, 
-            992: { 
+            },
+            992: {
               slidesPerView: 5,
             },
           }}
@@ -63,55 +62,56 @@ const CoverflowCarousel = () => {
           }}
           className="swiper coverflow-slider !py-5"
         >
-          {userData &&  userData.map((user) => {
-            const {UserName, Photo, Cover,CreatedAt, id}= user; 
-            return (
-              <SwiperSlide key={id}>
-                <article>
-                  <div className="block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700">
-                    <figure className="relative">
-                      <Link href={"/user/" + id}>
-                        <a>
-                          <Image
-                            src={Cover ? Cover : Photo}
-                            alt={UserName}
-                            className="swiper-lazy h-[430px] w-full object-cover"
-                            height="430"
-                            width="379"
-                          />
-                        </a>
-                      </Link>
-                    </figure>
-                    <div className="p-6">
-                      <div className="flex">
+          {userData &&
+            userData.map((user) => {
+              const { UserName, Photo, Cover, CreatedAt, id } = user;
+              return (
+                <SwiperSlide key={id}>
+                  <article>
+                    <div className="block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700">
+                      <figure className="relative">
                         <Link href={"/user/" + id}>
-                          <a className="shrink-0">
+                          <a>
                             <img
                               src={Photo}
                               alt="avatar"
-                              className="mr-4 h-10 w-10 rounded-full"
+                              className="swiper-lazy h-[430px] w-full object-cover"
+                              height="300"
+                              width="379"
                             />
                           </a>
                         </Link>
-                        <div>
+                      </figure>
+                      <div className="p-6">
+                        <div className="flex">
                           <Link href={"/user/" + id}>
-                            <a className="block">
-                              <span className="font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white">
-                                {UserName}
-                              </span>
+                            <a className="shrink-0">
+                              <img
+                                src={Photo}
+                                alt="avatar"
+                                className="mr-4 h-10 w-10 rounded-full"
+                              />
                             </a>
                           </Link>
-                          <Link href={"/user/" + id}>
-                            <a className="text-2xs text-accent">{UserName}</a>
-                          </Link>
+                          <div>
+                            <Link href={"/user/" + id}>
+                              <a className="block">
+                                <span className="font-display text-lg leading-none text-jacarta-700 hover:text-accent dark:text-white">
+                                  {UserName}
+                                </span>
+                              </a>
+                            </Link>
+                            <Link href={"/user/" + id}>
+                              <a className="text-2xs text-accent">{UserName}</a>
+                            </Link>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </article>
-              </SwiperSlide>
-            );
-          })}
+                  </article>
+                </SwiperSlide>
+              );
+            })}
         </Swiper>
 
         <div className="swiper-button-prev-4 group absolute top-1/2 left-4 z-10 -mt-6 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-white p-3 text-jacarta-700 text-xl shadow-white-volume">
